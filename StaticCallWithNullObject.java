ab075131c7046f49d699d0cb7b23ee47573721e4
avakesh <43170506+avakesh@users.noreply.github.com>
2018-09-11 15:49:19 +0530

    Update StaticCallWithNullObject.java

--- StaticCallWithNullObject.java
+++ StaticCallWithNullObject.java
@@ -1,1 +1,16 @@
 
+public class StaticCallWithNullObject {
+    //static method 
+    public static void getEmployeeName(){
+        System.out.println("Avakesh Kumar");
+    }
+    
+    public static void main(String av[]){
+        //declare object with null
+        StaticCallWithNullObject staticCallWithNullObject = null;
+        //call static method with null object
+        staticCallWithNullObject.getEmployeeName();
+    }
+}
+
+//output -- It will print 'Avakesh Kumar'.
